466022688bf4e2c567e25e553121883d14c1735b
rsato <sato.ryuta.main@gmail.com>
2023-01-25 05:05:47 +0900

    :hammer: Upgrade play_cards method

--- millionaire/libs/match/player.py
+++ millionaire/libs/match/player.py
@@ -10,7 +10,9 @@ class Player:
         self.passedはプライベート属性に変更予定です．
         各ターンのカードの選択はこのクラスが単独で行います．
     """
-    def __init__(self, uuid: UUID, name: str):
+
+    def __init__(self, uuid: UUID, name: str, __ws=None,):
+        self.__ws = __ws
         self.uuid = uuid
         self.name = name
         # self.connection_status = ???
@@ -20,23 +22,25 @@ class Player:
     def __len__(self):
         return self.cards.__len__()
 
+    def play_cards(self, played_cards: Cards = None) -> Cards | None:
+        """ played_cardsに基づいて，出せる候補の中から１つを返します．
 
-    def play_cards(self, played_cards: list[Card] | None):
-        """
+        カードを出せる場合に，Cardsクラスのインスタンスを返します．
+        返せない場合はNoneを返します．
+
         Args:
-            played_cards:
+            played_cards(Cards):
                 最新の場のカードのリスト，ない場合は None
         Returns:
-            list: one of candidate cards sets
-            empty list: DEPRECATED
-            TODO: None: FUTURE REPLACE WITH []
+            Cards: one of candidate cards sets
+            None: means that the player selected pass operation
         """
         if self.passed:
-            return []
+            return None
         candidate_cards_set = self.cards.lookfor_candidate_cards_set(played_cards)
         if candidate_cards_set:
             return candidate_cards_set[0]
-        return []
+        return None
 
 
 if __name__ == '__main__':
