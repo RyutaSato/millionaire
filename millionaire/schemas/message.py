3caa04ea130f718323866b14840d6dd30500b58a
rsato <sato.ryuta.main@gmail.com>
2023-02-02 01:46:17 +0900

    :bug: fixed wrong positions of Literal[key]

--- millionaire/schemas/message.py
+++ millionaire/schemas/message.py
@@ -3,24 +3,29 @@ from pydantic import BaseModel, Field
 from uuid import UUID
 from datetime import datetime
 
-from millionaire.schemas.msg_types import MsgTypes, StatusTypes
+from millionaire.schemas.msg_types import MsgTypes, PlayMsgTypes, StatusTypes
 
 
 class NoneMessage(BaseModel):
-    msg_type: MsgTypes = Literal['none']
+    msg_type: Literal['none']
     body: str
 
 
 class InPlayMessage(BaseModel):
-    msg_type: MsgTypes = Literal['in_play']
+    msg_type: Literal['in_play']
+    play_type: PlayMsgTypes
+    cards: list[str] = []
 
 
 class OutPlayMessage(BaseModel):
-    msg_type: MsgTypes = Literal['out_play']
+    msg_type: Literal['out_play']
+    play_type: PlayMsgTypes
+    cards: list[str] = []
+
 
 
 class RoomMessage(BaseModel):
-    msg_type: MsgTypes = Literal['room']
+    msg_type: Literal['room']
     status: StatusTypes
 
 
@@ -35,7 +40,7 @@ class Message(BaseModel):
     uid: UUID
     created_by: str  # class名
     created_at: datetime = Field(default_factory=datetime.utcnow)
-    msg_type: MsgTypes = MsgTypes.none
+    msg_type: MsgTypes = Literal['none']
     msg: InPlayMessage | OutPlayMessage | RoomMessage = Field(discriminator="msg_type")
 
     class Config:
