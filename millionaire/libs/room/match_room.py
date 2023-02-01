659167a17d44a03cdcd68e4676ebae66f396549b
rsato <sato.ryuta.main@gmail.com>
2023-02-02 01:52:21 +0900

    :zap: linked MatchRoom with Play class

--- millionaire/libs/room/match_room.py
+++ millionaire/libs/room/match_room.py
@@ -1,6 +1,7 @@
 import asyncio
 from uuid import UUID
 
+from millionaire.libs.match.play import Play
 from millionaire.libs.room.baseroom import BaseRoom, RoomType
 from millionaire.schemas.message import Message
 import logging
@@ -14,6 +15,7 @@ class MatchRoom(BaseRoom):
         super().__init__(RoomType.match, room_manager)
         for uid in uids:
             self.__manager.move_user(uid, self.room_id)
+        self.__play = Play.from_user_manager(users=list(self.__roommates.values()))
         self.play_task = asyncio.create_task(self.init_play())
         # TODO: 試合終了後のコールバックを書く
 
